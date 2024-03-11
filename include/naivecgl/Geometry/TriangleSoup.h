49171f3774b9a92d1f9682d979811b142fd3320a
AnthonyK213 <tjjzqwh@163.com>
2024-03-11 17:23:28 +0800

    fix

--- include/naivecgl/Geometry/TriangleSoup.h
+++ include/naivecgl/Geometry/TriangleSoup.h
@@ -12,8 +12,6 @@ class TriangleSoup : std::enable_shared_from_this<TriangleSoup> {
 public:
   Naive_EXPORT TriangleSoup();
 
-  Naive_EXPORT TriangleSoup(TriangleSoup &&theOther) noexcept;
-
   template <typename V, typename T,
             typename = Naive_WHERE_IS_REF_OF(V, VertexList),
             typename = Naive_WHERE_IS_REF_OF(T, TriangleList)>
@@ -22,16 +20,12 @@ public:
     myTriangles = std::forward<T>(theTriangles);
   }
 
-  Naive_EXPORT TriangleSoup &operator=(TriangleSoup &&theOther) noexcept;
-
-  Naive_EXPORT ~TriangleSoup();
-
 public:
   Naive_EXPORT Naive_Bool IsValid();
 
-  const VertexList &Vertices() const { return myVertices; }
+  Naive_EXPORT const VertexList &Vertices() const { return myVertices; }
 
-  const TriangleList &Triangles() const { return myTriangles; }
+  Naive_EXPORT const TriangleList &Triangles() const { return myTriangles; }
 
 private:
   VertexList myVertices;
--- src/Geometry_TriangleSoup.cpp
+++ src/Geometry_TriangleSoup.cpp
@@ -4,20 +4,6 @@ Naive_NAMESPACE_BEGIN(geometry);
 
 TriangleSoup::TriangleSoup() : myVertices(), myTriangles() {}
 
-TriangleSoup::TriangleSoup(TriangleSoup &&theOther) noexcept {
-  myVertices = std::move(theOther.myVertices);
-  myTriangles = std::move(theOther.myTriangles);
-}
-
-TriangleSoup &TriangleSoup::operator=(TriangleSoup &&theOther) noexcept {
-  myVertices = std::move(theOther.myVertices);
-  myTriangles = std::move(theOther.myTriangles);
-
-  return *this;
-}
-
-TriangleSoup::~TriangleSoup() {}
-
 Naive_Bool TriangleSoup::IsValid() {
   Naive_Size nbVertices = myVertices.size();
 
